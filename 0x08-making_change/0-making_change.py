c07eb8a9c9fb2833a328aa4efc43ba0496b35c30
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-09-19 20:44:23 +0000

    Add and fix some tasks

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Change making module.
+"""
+
+
+def makeChange(coins, total):
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
+    if total <= 0:
+        return 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for i in range(1, total + 1):
+        for j in range(0, len(coins)):
+            if coins[j] <= i:
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+    return -1 if dp[total] > total else dp[total]
